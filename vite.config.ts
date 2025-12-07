1a145680b43a4272edc048ed20c4734072f2e950
Nexcubic <sanjays@nexcubic.com>
2025-12-07 12:46:27 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    base: "/shadcn-nextra-header"
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
